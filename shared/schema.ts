9a26da4642376db2bd7a62846f420fa9906e32cc
Skafeer <skafeerr@gmail.com>
2026-06-07 09:44:28 +0000

    fix:  Some improvements and bug fixes

--- shared/schema.ts
+++ shared/schema.ts
@@ -41,7 +41,6 @@ export const orders = pgTable("orders", {
   merchantId: integer("merchant_id").notNull(),
   customerName: text("customer_name").notNull(),
   customerPhone: text("customer_phone").notNull(),
-  backupPhone: text("backup_phone"), // ✅ تمت إضافة الرقم الاحتياطي
   province: text("province").notNull(),
   address: text("address").notNull(),
   notes: text("notes").notNull().default(""),
